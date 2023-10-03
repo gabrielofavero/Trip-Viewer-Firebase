4576caf0bcda3b18222b0371b9f8259ba13f4ce1
Gabriel Favero <gabriel.o.favero@live.com>
2023-10-03 20:15:37 -0300

    Keypoints Firestore Impl

--- public/assets/js/modules/information/keypoints.js
+++ public/assets/js/modules/information/keypoints.js
@@ -1,94 +1,35 @@
 // ======= Keypoints JS =======
 
 function _loadKeypointsStandAlone() {
-    const KEYPOINTS = CONFIG.information.keypoints;
-    let DADO4;
-    switch (KEYPOINTS[3].titulo) {
-        case "Valor Previsto":
-            DADO4 = _removeComma(SHEET_TOTAL);
-            break;
-        case "Dia-A-Dia":
-            DADO4 = _removeComma(SHEET_DAY_TO_DAY[6][2]);
-    }
+    const inicioDate = _convertFirestoreDate(FIRESTORE_DATA.inicio);
+    const fimDate = _convertFirestoreDate(FIRESTORE_DATA.fim);
+    const dias = Math.ceil((fimDate - inicioDate) / (1000 * 60 * 60 * 24));
 
+    const inicio = `${inicioDate.getDate()}/${inicioDate.getMonth() + 1}`;
+    const fim = `${fimDate.getDate()}/${fimDate.getMonth() + 1}`;
+    const pessoas = FIRESTORE_DATA.quantidadePessoas;
+    
     // Dado 1
     document.getElementById("dado1").innerHTML = `
-  <i class="${KEYPOINTS[0].icone}"></i>
-  <span>${KEYPOINTS[0].texto}</span>
-  <p>${KEYPOINTS[0].titulo}</p>`;
+  <i class="bx bxs-plane-take-off"></i>
+  <span>${inicio}</span>
+  <p>Ida</p>`;
 
     // Dado 2
     document.getElementById("dado2").innerHTML = `
-  <i class="${KEYPOINTS[1].icone}"></i>
-  <span>${KEYPOINTS[1].texto}</span>
-  <p>${KEYPOINTS[1].titulo}</p>`;
+  <i class="bx bxs-plane-land"></i>
+  <span>${fim}</span>
+  <p>Volta</p>`;
 
     // Dado 3
     document.getElementById("dado3").innerHTML = `
-  <i class="${KEYPOINTS[2].icone}"></i>
-  <span>${KEYPOINTS[2].texto}</span>
-  <p>${KEYPOINTS[2].titulo}</p>`;
+  <i class="bx bxs-sun"></i>
+  <span>${dias}</span>
+  <p>Dias de Viagem</p>`;
 
     // Dado 4
     document.getElementById("dado4").innerHTML = `
-  <i class="${KEYPOINTS[3].icone}"></i>
-  <span>${DADO4 || KEYPOINTS[3].texto}</span>
-  <p>${KEYPOINTS[3].titulo}</p>`
-}
-
-function _loadKeypointsIntegrated() {
-
-    for (let i = 0; i < KEYPOINTS_JSON.length; i++) {
-        document.getElementById(`dado${i + 1}`).innerHTML = _getKeypoint(KEYPOINTS_JSON[i]);
-    }
-
-    // Data Update
-    KEYPOINTS_JSON[1].titulo = TRANSPORTATION_JSON["defaultTransportation"]["default"]["text"];
-    KEYPOINTS_JSON[1].icone = TRANSPORTATION_JSON["defaultTransportation"]["default"]["icon"];
-}
-
-function _getKeypoint(dado) {
-    let titulo = dado.titulo || "Não Definido";
-    let icone = dado.icone || "bx question-mark";
-    let texto = dado.texto || "-";
-
-    switch (dado.titulo) {
-        case "Gastos Durante a Viagem":
-        case "Gastos na Viagem":
-            // Ajustes
-            let valor = COST_DURING_TRIP;
-            if (ADJUSTMENTS["Total"] || ADJUSTMENTS["Ambos"]) {
-                valor = ADJUSTED_COST_DURING_TRIP;
-            }
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(valor));
-            break;
-        case "Ida e Volta":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(TRANSPORTATION_COST));
-            break;
-        case "Hospedagem":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(STAY_COST));
-            break;
-        case "Dia-a-Dia":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(DAY_TO_DAY_COST) * NUMBER_OF_PEOPLE);
-            break;
-        case "Dia-a-Dia (Individual)":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(DAY_TO_DAY_COST));
-            break;
-        case "Ingresso":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(TICKET_COST));
-            break;
-        case "Ingressos":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(TICKET_COST) * NUMBER_OF_PEOPLE);
-            break;
-        case "Gastos Prévios":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(PREVIOUS_COST));
-            break;
-        case "Gasto Total":
-        case "Total":
-            texto = (texto != "-") ? texto : `${FIRESTORE_DATA.moeda} ` + _addDotSeparator(Math.round(TOTAL_COST));
-            break;
-    }
-    return `<i class="${icone}"></i>
-            <span>${texto}</span>
-            <p>${titulo}</p>`;
+  <i class="bx bx-male"></i>
+  <span>${pessoas}</span>
+  <p>Pessoas Viajando</p>`
 }
