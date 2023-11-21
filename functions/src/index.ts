2afa6df96ac609fcec96adec52a4348fb83c64a7
Gabriel Favero <gabriel.o.favero@live.com>
2023-11-20 21:56:53 -0300

    exported places list

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import { getBackup } from "./data/main/backup";
 import { getConfig, getSingleTrip, getTripList } from "./data/viagem/get";
 import { updateTrip, newTrip } from "./data/viagem/set";
+import { getPlacesList } from "./data/passeios/get";
 import * as admin from "firebase-admin";
 import credentials from "./data/main/credentials";
 
@@ -13,6 +14,7 @@ export {
   getConfig,
   getSingleTrip,
   getTripList,
+  getPlacesList,
   updateTrip,
   newTrip,
 };
