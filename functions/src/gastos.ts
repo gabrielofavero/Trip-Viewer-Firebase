f35540eff19174151300e3eff1b07421f7e007f2
Gabriel Favero <gabriel.o.favero@live.com>
2024-06-07 13:24:30 -0300

    (E015) Função de Backend e Frontend

---
+++ functions/src/gastos.ts
@@ -0,0 +1,39 @@
+import { onRequest } from "firebase-functions/v2/https";
+import * as bcrypt from "bcrypt";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+export const getGastos = onRequest(async (request, response) => {
+    const pin = request.body.pin as string;
+    const documentID = request.body.documentID as string;
+
+    if (!documentID) {
+        response.status(400).json({ error: "O ID do documento não foi fornecido." });
+        return;
+    }
+
+    try {
+        const documentRef = admin.firestore().doc(`gastos/${documentID}`);
+        const documentSnapshot = await documentRef.get();
+
+        if (!documentSnapshot.exists) {
+            response.status(404).json({ error: "Documento não encontrado." });
+            return;
+        }
+        const documentData = documentSnapshot.data();
+        const storedPin = documentData?.pin;
+
+        if (storedPin) {
+            const isPinValid = await bcrypt.compare(pin, storedPin);
+            if (!isPinValid) {
+                response.status(403).json({ error: "PIN inválido." });
+                return;
+            }
+        }
+        response.json(documentData);
+    } catch (error) {
+        console.error("Erro ao acessar o Firestore:", error);
+        response.status(500).json({ error: "Erro interno do servidor." });
+    }
+});
---
+++ public/assets/js/paginas/editar-viagem/suporte/security.js
@@ -0,0 +1,27 @@
+async function generateHash(password) {
+    // https://www.toptal.com/developers/bcrypt/
+    try {
+        const COST = 10;
+        const url = 'https://www.toptal.com/developers/bcrypt/api/generate-hash.json';
+    
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `password=${password}&cost=${COST}`
+        });
+    
+        const data = await response.json();
+    
+        if (data.ok) {
+            return data.hash;
+        } else {
+            console.error('Não foi possível gerar o hash da senha: ' + data.msg);
+            _displayErrorMessage(data.msg);
+        }
+    } catch (error) {
+        console.error('Erro ao gerar hash da senha: ' + error);
+        _displayErrorMessage(error);
+    }
+}
