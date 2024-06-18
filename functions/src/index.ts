9ac935975a2a406bab66da9e343887d0c850e69f
Gabriel Favero <gabriel.o.favero@live.com>
2024-06-18 13:09:16 -0300

    (E015) Remoção cloud functions de get e set gastos de edição

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,5 +3,3 @@ import * as gastos from "./gastos";
 
 export const convertTikTokLinks = tiktok;
 export const getGastos = gastos.getGastos;
-export const getGastosEdit = gastos.getGastosEdit;
-export const setGastosEdit = gastos.setGastosEdit;
