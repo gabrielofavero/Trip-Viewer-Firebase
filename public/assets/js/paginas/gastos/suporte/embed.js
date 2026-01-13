147ee3220dd8e7f7e7a9d1e1a98c1e0ac7530c39
Gabriel Favero <gabriel.o.favero@live.com>
2026-01-13 00:11:46 -0300

    removed custom background

--- public/assets/js/paginas/gastos/suporte/embed.js
+++ public/assets/js/paginas/gastos/suporte/embed.js
@@ -4,22 +4,13 @@ function _loadEmbedMode(visibility) {
     document.querySelector('.top-bar').style.display = 'none'
     document.querySelector('.section-title').style.display = 'none'
     document.querySelector('.footer').style.display = 'none';
-    _loadEmbedVisibility(visibility);
+    _loadExternalVisibility(visibility);
 }
 
 function _onViewMessage(data) {
     switch (data.type) {
         case 'visibility':
             _loadExternalVisibility(data.value);
-    }
-}
-
-function _loadEmbedVisibility(visibility) {
-    visibility = visibility || _getVisibility();
-    _loadExternalVisibility(visibility);
-    const preloader = getID('preloader');
-    if (preloader.style.display == 'block' && preloader.querySelector('.input-container')) {
-        preloader.style.background = _isOnDarkMode() ? 'rgba(64, 64, 64, 0.6)' : 'rgba(241 241 241, 0.6)'
     }
 }
 
